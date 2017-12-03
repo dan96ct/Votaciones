18fac9f935e259a67ba61f25ecce57518bb32c62
dani <basketanbasket@gmail.com>
2017-12-03 13:54:32 +0100

    Creacion del proyecto

---
+++ build/web/index.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : index
+    Created on : 03-dic-2017, 11:51:08
+    Author     : dani
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+        <link href="Vistas/css/css.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <%@include file="Vistas/menu.jsp" %>
+    </body>
+</html>
---
+++ src/java/DAO/Conexion_bd.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import com.sun.rowset.CachedRowSetImpl;
+import java.beans.Statement;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.rowset.CachedRowSet;
+
+/**
+ *
+ * @author dani
+ */
+public class Conexion_bd extends HttpServlet {
+
+    private String url = "jdbc:mysql://localhost/bd_votaciones";
+    private String login = "root";
+    private String password = "root";
+
+    public void conexionBD() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        String connectionUrl = "jdbc:mysql://localhost/ejercicio";
+        Connection con = DriverManager.getConnection(connectionUrl, login, password);
+        
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Conexion_bd</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Conexion_bd at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
